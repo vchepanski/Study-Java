a4bae77fc6356d5fafa3072160aa2c39e09b511d
vchepanski <vchepanski@outlook.com>
2026-02-19 17:59:59 -0300

    feat: Add EstudoNovo and Product classes

--- src/main/java/com/example/demo/EstudoNovo.java
+++ src/main/java/com/example/demo/EstudoNovo.java
@@ -3,19 +3,34 @@ package com.example.demo;
 import java.util.Locale;
 import java.util.Scanner;
 
+import entities.Product;
+
 public class EstudoNovo {
     public static void main(String[] args) {
-       Locale.setDefault(Locale.US);
-       Scanner sc = new Scanner(System.in);
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
 
-       int vezes = sc.nextInt();
-       double[] array = new double[vezes];
-       
-       for (int i = 0; i < n; i++){
-            array[i] = sc.nextDouble();
-       }
+        System.out.println("Digite a quantidade de produtos");
+        int n = sc.nextInt();
 
+        Product[] vect = new Product[n];
+        
+        for (int i=0; i<vect.length; i++) {
+            System.out.println("Digite o nome do produto");
+            sc.nextLine();
+            String name = sc.nextLine();
+            System.out.println("Digite o valor do produto");
+            double price = sc.nextDouble();        
+            vect[i] = new Product(name, price);
+        }
 
-       sc.close();
+        double sum = 0.0;
+
+        for (int i=0; i<vect.length; i++) {
+            sum += vect[i].getPrice();
+        }
+        double avg = sum / vect.length;
+        System.out.printf("AVERAGE PRICE = %.2f%n", avg);
+        sc.close();
     }
 }
